58fe1030246d69526fc0746bdac4590ac5d95973
mahmud <abubakarmahmud376@gmail.com>
2023-03-10 00:23:19 +0100

    m

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    args = sys.argv
-    args.pop(0)
-    argc = len(args)
-    line1 = "{:d} argument".format(argc)
-    if argc == 0:
+    arbs = sys.argv
+    arbs.pop(0)
+    arbc = len(args)
+    line1 = "{:d} argument".format(arbc)
+    if arbc == 0:
         line1 += "s."
-    elif argc == 1:
+    elif arbc == 1:
         line1 += ":"
     else:
         line1 += "s:"
     print(line1)
-    for i, arg in enumerate(args):
+    for i, arg in enumerate(arbs):
         print("{:d}: {:s}".format(i+1, arg))
