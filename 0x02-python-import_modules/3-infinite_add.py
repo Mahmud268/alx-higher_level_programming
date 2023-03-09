f21b74534fe64c0de00c54cb64b4d25dd1e7ee74
mahmud <abubakarmahmud376@gmail.com>
2023-03-09 23:42:01 +0100

    m

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__import__":
+    import sys
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
