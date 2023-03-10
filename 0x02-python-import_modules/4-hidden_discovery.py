bd0a40c3e9a5ed1cef4f3493bdcd6317161880ef
mahmud <abubakarmahmud376@gmail.com>
2023-03-10 01:06:26 +0100

    m

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4 as h
-if __name__ == '__main__':
-    names = filter(lambda n: not n.startswith('__'), dir(h))
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
     for name in names:
-        print(name)
+        if name[:2] != "__":
+            print(name)
