537dcfede19e5d93bdfd87f0ae57a9b3c12e8215
Mahmud <abubakarmahmud376@gmail.com>
2023-03-13 16:45:11 +0100

    m

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
+    """
+    Return a copy of my string without c or C
+    Args:
+        my_string - the string to filter
+
+    """
     new_string = ''
     for char in my_string:
         if char is not in ['c', 'C']:
