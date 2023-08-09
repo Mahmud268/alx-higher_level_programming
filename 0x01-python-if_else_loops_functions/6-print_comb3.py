f5f70f58c664dc5b7344d03cf5d2b36fd23ae807
Mahmud <abubakarmahmud376@gmail.com>
2023-08-09 22:06:13 +0100

    possible combination of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,7 @@ for i in range(10):
     for j in range(1, 10):
         if i >= j:
             continue
+        if j == 9 and i == 8:
             print("{:d}{:d}".format(i, j))
         else:
             print("{:d}{:d}".format(i, j), end=", ")
