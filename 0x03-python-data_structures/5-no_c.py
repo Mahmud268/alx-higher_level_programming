2f5d170aaeae1dc2479dfe210755228892bb7298
Mahmud <abubakarmahmud376@gmail.com>
2023-08-14 23:53:25 +0000

    filters the character 'cc'

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    for char in range(len(my_string)):
-        if char == 'c' or char == 'C':
-            print("{}".format(my_list[char]))
+    return "".join(filter(lambda x: x not in 'cC', my_string))
