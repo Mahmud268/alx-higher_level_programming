dce76b25b6af1b48cf8fc46391d753fa5a1d68d5
Mahmud <abubakarmahmud376@gmail.com>
2023-09-11 18:08:10 +0100

    A fuction that inherits from a class list and print the sorted element

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A defination of class Mylist that inherits from the class list"""
+
+class MyList(List):
+    """A defination of class mylist that inherits from the class list"""
+
+    def print_sorted(self):
+        """Print the element of the list in assending order"""
+        return sorted(self)
