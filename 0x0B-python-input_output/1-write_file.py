01c9fb97ff26f81033f59a2bbea1c93d820d1030
Mahmud <abubakarmahmud376@gmail.com>
2023-09-12 21:37:59 +0100

    my second task

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A module that contain a fuction that write file"""
+
+
+def write_file(filename="", text=""):
+    """A fuction that write a string to a filename.
+
+    Args:
+        filename: a to be written to.
+        text: a text to be added to file.
+
+    Returns:
+        None
+    """
+
+        with open(filename, w, encoding="utf-8") as f:
+            return f.write(text)
