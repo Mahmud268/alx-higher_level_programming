acc646c6884866c8a22fb3783666105aad16f0e5
Mahmud <abubakarmahmud376@gmail.com>
2023-09-12 21:14:30 +0100

    my first task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,8 +3,7 @@
 """Contain function that read a text file"""
 
 def read_file(filename=""):
-    """A defination of function that read a text file"""
+    """A defination of function that read UTF8 text file and print it to stdout"""
 
     with open(filename, encoding="utf-8") as f:
-        read = f.read()
-    print(read)
+        print(f.read(), end"")
