66383fcc5024e39789aaab692529d5a55b385791
Mahmud <abubakarmahmud376@gmail.com>
2023-09-12 21:05:34 +0100

    my first task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,6 @@
 def read_file(filename=""):
     """A defination of function that read a text file"""
 
-    with open('UTF8', encoding="utf-8") as f:
-        f.read()
+    with open(filename, encoding="utf-8") as f:
+        read = f.read()
+    return read
